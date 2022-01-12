91d4f2bbee0ebd2c1b7da28204e71ae9ff9a0ac6
June Tien <chiachun.tien@gmail.com>
2022-01-13 00:50:36 +0800

    temp

--- src/flatten/question.js
+++ src/flatten/question.js
@@ -3,16 +3,44 @@
  * 參數可以傳入任意維度的陣列，扁平化後回傳一維陣列
  * flatten([1, [2, [3, [4]]], 5])
  * => [1, 2, 3, 4, 5]
+ * [3, [4]]
  */
+const result = [];
 export const flattenDeep = (array) => {
   // 請在這裡實作
-  const result = [];
+  // if (array.length) {
+  //   flattenDeep(array);
+  // } else {
+  //   return array;
+  // }
+  // if (array.length) {
+  //   array.forEach((item) => {
+  //     if (item.length > 0) {
+  //       flattenDeep(item);
+  //     } else {
+  //       result.push(item);
+  //     }
+  //   });
+  // } else {
+  //   result.push(array);
+  // }
+  // array.forEach((item) => {
+  //   if (item.length > 0) {
+  //     flattenDeep(item);
+  //   } else {
+  //     result.push(item);
+  //   }
+  // });
+  exec(array);
+  return result;
+};
+
+const exec = (array) => {
   array.forEach((item) => {
     if (item.length > 0) {
-      flattenDeep(item);
+      exec(item);
     } else {
       result.push(item);
     }
   });
-  return result;
 };
